e6f410dfc82d5865cfd25cf0ca92a5d72896c228
Zajas Piotr <zajas.piotr@gmail.com>
2023-01-06 20:23:09 +0100

    Create an action to clear the todo list.

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,15 +1,25 @@
-import { useSelector } from "react-redux"
+import { useDispatch, useSelector } from "react-redux"
 import { InterfaceState, InterfaceTodo } from "../interfaces/todoInterface"
+import { clearTodos } from "../redux/slice/todoSlice"
 
 import TodoItem from "./TodoItem"
 
 const TodoList = () => {
   const todos = useSelector((state: InterfaceState) => state.todos)
+
+  const dispatch = useDispatch()
+
+  const handleDeleteUserTodos = () => {
+    dispatch(clearTodos())
+  }
   return (
     <div>
-      {todos.map((todo: InterfaceTodo) => (
-        <TodoItem todo={todo} key={todo.id} />
-      ))}
+      <div>
+        {todos.map((todo: InterfaceTodo) => (
+          <TodoItem todo={todo} key={todo.id} />
+        ))}
+      </div>
+      <button onClick={handleDeleteUserTodos}>Delete All</button>
     </div>
   )
 }
