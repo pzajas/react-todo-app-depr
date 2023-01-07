834347077df9df65630a9093afe4b45c2bde832c
Zajas Piotr <zajas.piotr@gmail.com>
2023-01-07 22:08:03 +0100

    Create a dynamic search filter to filter todos by user input.

--- src/redux/store/store.ts
+++ src/redux/store/store.ts
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit"
 import todoReducer from "../slice/todoSlice"
+import searchReducer from "../slice/searchSlice"
 
 export const store = configureStore({
   reducer: {
     todos: todoReducer,
+    search: searchReducer,
   },
 })
