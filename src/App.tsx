6f256d8a70095ede4a0ca01e8524b08b9c4b97eb
Zajas Piotr <zajas.piotr@gmail.com>
2023-01-09 23:10:43 +0100

    Create the todo header component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import TodoForm from "./components/TodoForm"
+import TodoHeader from "./components/TodoHeader"
 import TodoList from "./components/TodoList"
 
 const App = () => {
   return (
     <div>
+      <TodoHeader />
       <TodoForm />
       <TodoList />
     </div>
